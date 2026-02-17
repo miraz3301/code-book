d74132f6b74d2250f40cbae5feeb9d6aefc3e650
Md. Miraz Morshed <miraz.rzsian21@gmail.com>
2026-02-18 00:28:27 +0600

    Delete String %mod.cpp

--- String %mod.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-const int MOD = 1e9 + 7;
-int stringMod(const string &s) 
-{
-    long long result = 0;  // use long long to prevent intermediate overflow
-    for (char c : s)result = (result * 10 + (c - '0')) % MOD;
-    return (int)result;
-}
