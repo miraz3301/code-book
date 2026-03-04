992804e01278194ce64b24bf95ebbe81bb2412b9
Md. Miraz Morshed <miraz.rzsian21@gmail.com>
2026-03-04 10:33:58 +0600

    LCS(longest common subsequence) using dp
    Updated the LCS implementation to include a function that prints the longest common subsequence.

--- DP/LCS(longest common subsequence)[dp].cpp
+++ DP/LCS(longest common subsequence)[dp].cpp
@@ -2,7 +2,7 @@
 #define ll long long
 #define endl '\n'
 using namespace std;
-vector<vector<ll>>dp(1000,vector<ll>(1000,0));
+vector<vector<ll>>dp(1000,vector<ll>(100,0));
 ll LCS(string s1,string s2)
 {
     ll n=s1.size();
@@ -18,10 +18,33 @@ ll LCS(string s1,string s2)
             if(s1[i-1]==s2[j-1])dp[i][j]=dp[i-1][j-1]+1;
             else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             //cout<<dp[i][j]<<" ";
+
         }
         //cout<<endl;
     }
     return dp[n][m];
+
+}
+string Print_LCS(string s1,string s2)
+{
+    ll len=LCS(s1,s2);
+    string ans;
+    ll q=s1.size()+s2.size();
+    pair<ll,ll>cur_pos={s1.size(),s2.size()};
+    while(q--)
+    {
+        //cout<<cur_pos.first<<" "<<cur_pos.second<<endl;
+        if(!cur_pos.first or !cur_pos.second)break;
+        if(dp[cur_pos.first][cur_pos.second]!=max(dp[cur_pos.first-1][cur_pos.second],dp[cur_pos.first][cur_pos.second-1]))
+        {
+            ans+=s1[cur_pos.first-1];
+            cur_pos={cur_pos.first-1,cur_pos.second-1};
+        }
+        else if(dp[cur_pos.first][cur_pos.second]==dp[cur_pos.first-1][cur_pos.second]) cur_pos={cur_pos.first-1,cur_pos.second};
+        else cur_pos={cur_pos.first,cur_pos.second-1};
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
 
 }
 int main()
@@ -32,6 +55,6 @@ int main()
     string s1,s2;
     cin>>s1>>s2;
 
-    cout<<LCS(s1,s2)<<endl;
+    cout<<Print_LCS(s1,s2)<<endl;
 
 }
