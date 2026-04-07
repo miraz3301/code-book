453d66629a0d9843a5bb05f0aa1686978bcf75df
Md. Miraz Morshed <miraz.rzsian21@gmail.com>
2026-04-07 23:43:55 +0600

    0/1 Knapsack implementation

--- DP(0,1 Knapsack)/0,1 Knapsack.cpp
+++ DP(0,1 Knapsack)/0,1 Knapsack.cpp
@@ -6,66 +6,39 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    ll n;
-    cin>>n;
-    vector<pair<ll,ll>>wt_val(n+1);
-    for(ll i=1;i<=n;i++)cin>>wt_val[i].first>>wt_val[i].second;
+    cout.tie(nullptr);
+    
+    ll n,cap;
+    cin>>n>>cap;
+    vector<pair<ll,ll>>wt_value(n+1);
+    for(ll i=1;i<=n;i++)cin>>wt_value[i].first>>wt_value[i].second;
 
-    ll cap;
-    cin>>cap;
     ll dp[n+1][cap+1];
     for(ll i=0;i<=n;i++)for(ll j=0;j<=cap;j++)dp[i][j]=0;
 
-    for(ll x=1;x<=n;x++)
+    for(ll i=1;i<=n;i++)
     {
-        for(ll y=1;y<=cap;y++)
+        for(ll j=1;j<=cap;j++)
         {
-            if(y<wt_val[x].first)
-            {
-                dp[x][y]=dp[x-1][y];
-            }
-            else
-            {
-                    dp[x][y]=max(dp[x-1][y],dp[x-1][y-wt_val[x].first]+wt_val[x].second);
-            }
+            if(j<wt_value[i].first)dp[i][j]=dp[i-1][j];
+            else dp[i][j]=max(dp[i-1][j],dp[i-1][j-wt_value[i].first]+wt_value[i].second);
         }
     }
-
-    cout<<endl;
-    /// printing the DP grid
-    for(ll i=0;i<=n;i++)
-    {
-        for(ll j=0;j<=cap;j++)cout<<dp[i][j]<<" ";
-        cout<<endl;
-    }
-    cout<<endl;
-    /// printing the selected items
-    cout<<"Selected items: ";
-    pair<ll,ll>pos={n,cap};
+    cout<<dp[n][cap]<<endl;
+    
     ll q=n;
+    ll i=n,j=cap;
     while(q--)
     {
-        pair<ll,ll>dum3={0,0};
-        if(pos==dum3)break;
-        pair<ll,ll>dum1={pos.first-1,pos.second};
-        if(dp[pos.first][pos.second]==dp[dum1.first][dum1.second]) /// NEY NAI
+        if(!i or !j) break;
+        if(dp[i][j]==dp[i-1][j]) i--;  // NEY NAI
+        else                           // NISE
         {
-                pos= {pos.first-1,pos.second};
-        }
-        else /// NISE
-        {
-            cout<<"(item: "<<pos.first<<" value: "<<wt_val[pos.first].second<<")  ";
-            pos={pos.first-1,pos.second-wt_val[pos.first].first};
+            cout<<i<<"th item is taken. value: "<<wt_value[i].second<<endl;
+            j-=wt_value[i].first;
+            i--;
         }
     }
+    
 
 }
-/*
-
-4
-2 12
-1 10
-3 20
-2 15
-5
-*/
