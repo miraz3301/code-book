c81db9d87e63070bd2bab96bd0967274a0739e13
Md. Miraz Morshed <miraz.rzsian21@gmail.com>
2026-07-10 17:21:32 +0600

    Implement Segment Tree for sum operations

---
+++ Tree/Segment Tree/1. Segment Tree for the Sum.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define ld long long double
+using namespace std;
+ll arr[100009],seg[500009];
+void build_segTree(ll ind,ll left,ll right)
+{
+    if(left==right)
+    {
+        seg[ind]=arr[left];
+        return;
+    }
+    ll mid=(left+right)/2;
+    build_segTree(2*ind,left,mid);
+    build_segTree(2*ind+1,mid+1,right);
+    seg[ind]=seg[2*ind]+seg[2*ind+1];
+}
+void update_segTree(ll ind,ll left,ll right,ll pos,ll val)
+{
+    if(pos<left||pos>right)return;
+    if(left==right)
+    {
+        seg[ind]=val;
+        return;
+    }
+    ll mid=(left+right)/2;
+    update_segTree(2*ind,left,mid,pos,val);
+    update_segTree(2*ind+1,mid+1,right,pos,val);
+    seg[ind]=seg[2*ind]+seg[2*ind+1];
+}
+ll seg_sum(ll ind,ll left,ll right,ll b,ll e)
+{
+    if(left>=b&&right<=e)return seg[ind];
+    if(b>right||e<left)return 0;
+    ll mid=(left+right)/2;
+    ll ans1=seg_sum(2*ind,left,mid,b,e);
+    ll ans2=seg_sum(2*ind+1,mid+1,right,b,e);
+    return ans1+ans2;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ll n,q;
+    cin>>n>>q;
+    for(ll i=1;i<=n;i++)cin>>arr[i];
+    build_segTree(1,1,n);
+    while(q--)
+    {
+        ll x;
+        cin>>x;
+        if(x==1)
+        {
+            ll pos,val;
+            cin>>pos>>val;
+            update_segTree(1,1,n,pos+1,val);
+        }
+        else
+        {
+            ll l,r;
+            cin>>l>>r;
+            cout<<seg_sum(1,1,n,l+1,r)<<endl;
+        }
+    }
+}
