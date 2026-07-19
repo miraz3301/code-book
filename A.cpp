75e5d62a3d66df871dcd7ca2f781a48933a959d6
Md. Miraz Morshed <miraz.rzsian21@gmail.com>
2026-07-19 14:35:47 +0600

    Create A.cpp

---
+++ A.cpp
@@ -0,0 +1,7 @@
+Assalamualaikum @everyone, 
+
+Considering the academic pressure, contest deadline has been extended until the next class.
+Congratulations to everyone who has solved all the problems. Your effort and dedication are highly appreciated. Participants who have received a few wrong submissions are encouraged not to lose motivation. Please review your solutions, identify the mistakes, discuss with the mentors and continue trying. Consistent effort and persistence are key to improvement, and there is still sufficient time to perform well. Wishing everyone best of luck. We look forward to seeing your best performance.
+
+
+https://codeforces.com/contestInvitation/f3880f40a1a8af88aec7a23947e964a4082828c2
