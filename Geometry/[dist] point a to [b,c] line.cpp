31d3180e1f4a78e037a1dbadb064382cac921ced
Md. Miraz Morshed <miraz.rzsian21@gmail.com>
2026-08-25 18:16:59 +0600

    Fix input variable for coordinates in distance calculation

--- Geometry/[dist] point a to [b,c] line.cpp
+++ Geometry/[dist] point a to [b,c] line.cpp
@@ -1,5 +1,5 @@
         long double ax,ay,bx,by,cx,cy;
-        cin>>ax>>ax>>bx>>by>>cx>>cy;
+        cin>>ax>>ay>>bx>>by>>cx>>cy;
         long double x=cy-by;
         long double y=bx-cx;
         long double c=-(bx-cx)*by+(by-cy)*bx;
