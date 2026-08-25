8438f524928201e54e057863df3800efea037aeb
Md. Miraz Morshed <miraz.rzsian21@gmail.com>
2026-08-25 18:07:57 +0600

    Add distance calculation from point to line

---
+++ Geometry/[dist] point a to [b,c] line.cpp
@@ -0,0 +1,7 @@
+        long double ax,ay,bx,by,cx,cy;
+        cin>>ax>>ax>>bx>>by>>cx>>cy;
+        long double x=cy-by;
+        long double y=bx-cx;
+        long double c=-(bx-cx)*by+(by-cy)*bx;
+        long double dis=abs(x*ax+y*ay+c)/sqrt(x*x+y*y);
+        cout<<fixed<<setprecision(10)<<dis;
